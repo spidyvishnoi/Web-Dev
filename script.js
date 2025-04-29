011120b3ea8466553725b3b50028d700d38cdf2d
spidyvishnoi <parvvishnoi893@gmail.com>
2025-04-30 00:49:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+// // let x = 10;
+// // let y = 23;
+// // z = x+y;
+// // console.log(z); 
+// const a = 7;
+//  if(a>0){
+//     result = 'positive';
+//  }
+//  else{
+//     result = 'not positive';
+//  }
+//  document.writeln(result);
+//  console.log(result);
+
+
+//  let b = 23;
+//  let c = 12;
+//  console.log(b+c);
+//  console.log(b|c);
+//  console.log(b&c);
+//  console.log(b^c);
+//  console.log(b*c);
+//  console.log(b%c);
+
+
+//  var foo=1;
+//  var output = 'Output ';
+//  switch(foo)
+//  {
+//     case 0 : output+='so';
+//     case 1 : output += 'what ';
+//     output += 'is ';
+//     case 2 : output +='your ';
+//     case 3: output +='name ';
+//     case 4: output += '?';
+//     console.log(output); 
+//     break;
+//     case 5: output += '!';
+//     console.log(output);
+//     break;
+//     default:
+//         console.log('please sorry');
+
+//  }
+ // wap in js that displays the largest integer among two
+ // wap in js to sort three number
+ //
+ 
+let a=10;
+let b=14;
+
+if(x>y){
+   console.log('a is greater');
+}
+else{
+   console.log('b is greater ')
+}
+ 
+ 
+var x = 0;
+var y = -1;
+var z = 4;
+
+
+if (x > y && x > z) {
+    if (y > z) {
+        console.log(x + ", " + y + ", " + z);
+    } else {
+        console.log(x + ", " + z + ", " + y);
+    }
+} else if (y > x && y > z) {
+    if (x > z) {
+        console.log(y + ", " + x + ", " + z);
+    } else {
+        console.log(y + ", " + z + ", " + x);
+    }
+} else if (z > x && z > y) {
+    if (x > y) {
+        console.log(z + ", " + x + ", " + y);
+    } else {
+        console.log(z + ", " + y + ", " + x);
+    }
+} 
+ 
+ 
+
+// let bc = prompt("enter number");
+// alert('badmoshi na mitrrr');
+// confirm("check again");
+
+
+
+const r = new Date();
+console.log(r);
+console.log(r.toDateString());
+console.log(r.getTime());
